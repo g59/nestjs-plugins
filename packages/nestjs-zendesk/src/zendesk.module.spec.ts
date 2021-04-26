52df581d2793fdc40c96eed94a9167a56fac0264
Keisuke Kan <9renpoto@gmail.com>
2021-04-26 10:30:09 +0900

    fix: spec

--- packages/nestjs-zendesk/src/zendesk.module.spec.ts
+++ packages/nestjs-zendesk/src/zendesk.module.spec.ts
@@ -13,15 +13,56 @@ describe("ZendeskModule", () => {
 
   it("defined", () => expect(module).toBeDefined());
 
-  it("forRoot", () =>
-    expect(
-      ZendeskModule.forRoot({
-        username: "name",
-        token: "token",
-        remoteUri: "http://example.com",
-      })
-    ).toMatchSnapshot());
+  it("forRoot", () => {
+    const res = ZendeskModule.forRoot({
+      username: "name",
+      token: "token",
+      remoteUri: "http://example.com",
+    });
 
-  it("forRootAsync", () =>
-    expect(ZendeskModule.forRootAsync({})).toMatchSnapshot());
+    expect(res.exports).toHaveLength(1);
+    expect(res.imports).toBeUndefined();
+    expect(res.module).toBeDefined();
+    expect(res.providers).toHaveLength(1);
+  });
+
+  it("forRootAsync", () => {
+    const res = ZendeskModule.forRootAsync({});
+    expect(res.exports).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "inject": Array [
+            "ZENDESK_MODULE",
+          ],
+          "provide": "ZENDESK_TOKEN",
+          "useFactory": [Function],
+        },
+      ]
+    `);
+    expect(res.imports).toBeUndefined();
+    expect(res.providers).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "inject": Array [],
+          "provide": "ZENDESK_MODULE",
+          "useFactory": [Function],
+        },
+        Object {
+          "inject": Array [
+            Array [],
+          ],
+          "provide": undefined,
+          "useClass": undefined,
+        },
+        Object {
+          "inject": Array [
+            "ZENDESK_MODULE",
+          ],
+          "provide": "ZENDESK_TOKEN",
+          "useFactory": [Function],
+        },
+      ]
+    `);
+    expect(res.module).toBeDefined();
+  });
 });
