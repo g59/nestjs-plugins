53d24fbab9219e8be126facb3655da9fa3f21df2
Keisuke Kan <9renpoto@gmail.com>
2021-11-22 11:49:34 +0900

    fix: format

--- packages/nestjs-firebase/__tests__/getFirebaseAdmin.test.ts
+++ packages/nestjs-firebase/__tests__/getFirebaseAdmin.test.ts
@@ -3,9 +3,8 @@ import { getFirebaseAdmin } from "../src/util/getFirebaseAdmin";
 describe("getFirebaseAdmin", () => {
   it("returns firebase admin client", () =>
     expect(
-    getFirebaseAdmin({
-      googleApplicationCredential: undefined,
-    })
-    ).toBeTruthy()
-  );
+      getFirebaseAdmin({
+        googleApplicationCredential: undefined,
+      })
+    ).toBeTruthy());
 });
